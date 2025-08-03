e6d66a83a0fa891e6506dc8922bfe4d58a23f368
suriya-js2705 <suriyajs1322@gmail.com>
2025-08-04 03:25:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+      base: "/madurai-demo-client/",
+  plugins: [react()],
+
     host: "::",
     port: 8080,
   },
